f4b5bc1a55da4b8c87526e13effdaa2a55d5f133
duyi <1079240024@qq.com>
2020-09-15 19:51:50 +0800

    修改网络流行词汇不含解释

--- get_word/handle_wangluo.py
+++ get_word/handle_wangluo.py
@@ -7,7 +7,7 @@
 #         if position % 2 == 0:
 #             danci_list.append(text)
 #     print(danci_list)
-with open("../网络流行词汇(不含词)", "w", encoding="utf-8") as write_file:
+with open("../网络流行词汇(不含解释)", "w", encoding="utf-8") as write_file:
     for line in open("../网络流程词汇.txt"):
         line = line.strip()
         if "摘要: " in line:
